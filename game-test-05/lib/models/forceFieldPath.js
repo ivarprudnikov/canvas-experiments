91f1a975dc097be8a96dc1497df911f2921c7fca
ivarprudnikov <ivar.prudnikov@gmail.com>
2021-12-05 23:21:16 +0000

    draw dots along the path

--- game-test-05/lib/models/forceFieldPath.js
+++ game-test-05/lib/models/forceFieldPath.js
@@ -5,15 +5,35 @@ export function ForceFieldPath({force, points}) {
     this.force = force;
     this.points = points;
     this.pathColor = '#e882ff';
-    this.borderColor = '#ffc200';
+    this.borderColor = 'rgba(255,191,0,0.16)';
 
-    const obj = new Path2D();
+    this.obj = new Path2D();
     for (let i = 0; i < this.points.length; i++) {
-        if (i === 0) obj.moveTo(this.points[i].x, this.points[i].y);
-        else obj.lineTo(this.points[i].x, this.points[i].y);
+        if (i === 0) this.obj.moveTo(this.points[i].x, this.points[i].y);
+        else this.obj.lineTo(this.points[i].x, this.points[i].y);
     }
-    obj.closePath();
-    this.obj = obj;
+    this.obj.closePath();
+
+    this.dots = [];
+    for (let i = 1; i <= this.points.length; i++) {
+        const A = this.points[i - 1];
+        const B = this.points[i%this.points.length];
+        const dX = B.x - A.x;
+        const dY = B.y - A.y;
+        const AB = Math.sqrt(dX**2 + dY**2);
+        let segments = AB >> 4;
+        const X1 = dX / segments;
+        const Y1 = dY / segments;
+
+        let x = A.x;
+        let y = A.y;
+        while (segments > 0) {
+            this.dots.push({x,y});
+            x+=X1;
+            y+=Y1;
+            segments--;
+        }
+    }
 
     this.renderOn = (canvas2D) => {
         canvas2D.lineWidth = this.force;
@@ -22,6 +42,14 @@ export function ForceFieldPath({force, points}) {
         canvas2D.lineWidth = 1;
         canvas2D.strokeStyle = this.pathColor;
         canvas2D.stroke(this.obj);
+
+        for (const {x,y} of this.dots) {
+            canvas2D.beginPath();
+            canvas2D.arc(x, y, 5, 0, 2 * Math.PI);
+            canvas2D.strokeStyle = this.pathColor;
+            canvas2D.stroke();
+        }
+
     }
 
     this.getForceAtPosWithin = (pos, within) => {}
